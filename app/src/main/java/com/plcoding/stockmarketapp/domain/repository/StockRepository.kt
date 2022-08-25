81417688c3fe459c602118e331b86e98eaae117c
Elexandro Torres <elexandro.torres@gmail.com>
2022-08-25 17:12:29 -0300

    company info and day csv about stocks

--- app/src/main/java/com/plcoding/stockmarketapp/domain/repository/StockRepository.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/domain/repository/StockRepository.kt
@@ -1,12 +1,26 @@
 package com.plcoding.stockmarketapp.domain.repository
 
+import com.plcoding.stockmarketapp.data.remote.StockApi
+import com.plcoding.stockmarketapp.domain.model.CompanyInfo
 import com.plcoding.stockmarketapp.domain.model.CompanyListing
+import com.plcoding.stockmarketapp.domain.model.IntradayInfo
 import com.plcoding.stockmarketapp.util.Resource
 import kotlinx.coroutines.flow.Flow
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface StockRepository {
     suspend fun getCompanyListings(
         fetchFromRemote: Boolean,
         query: String,
     ) : Flow<Resource<List<CompanyListing>>>
+
+    suspend fun getIntradayInfo(
+        symbol : String,
+    ) : Resource<List<IntradayInfo>>
+
+    suspend fun getCompanyInfo(
+        symbol : String
+    ) : Resource<CompanyInfo>
 }
